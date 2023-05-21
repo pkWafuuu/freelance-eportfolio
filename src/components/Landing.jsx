3b8ac30fc016aba826dbae133b7d7edb0ee0e5f2
Phil <pk.oaferina@gmail.com>
2023-05-21 16:17:15 +0400

    add animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,5 @@
 import picture from "../assets/b.jpg";
+import Typical from "react-typical";
 
 function Landing() {
   return (
@@ -9,7 +10,10 @@ function Landing() {
             <img src={picture} alt="" className="header__picture" />
           </figure>
           <div className="header__content">
-            <h1 className="header__title">Hey, Justin here!</h1>
+            <h1 className="header__title">
+              Hey, Justin here!{" "}
+              <Typical loop={Infinity} steps={[1500, "", 1500, "Poet", 1500, "Writer", 2000, "Web Developer", 2000]} />
+            </h1>
             <p className="header__para">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus,
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi,
