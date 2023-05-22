513b6d1bceeb184045a8966b94802413f82bb462
Phil <pk.oaferina@gmail.com>
2023-05-22 20:06:49 +0400

    feat: add animations, style, and burger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-function Nav({ handleClick, works }) {
+function Nav({ handleClick, works, home }) {
+	const [load, setLoad] = useState(false)
+
+	function onClickMenu(){
+		setLoad((prev) => !prev)
+	}
+
   return (
     <nav>
       <div className="nav__row">
-        <div className="nav__logo">Justin Cruzana</div>
+        <div className="nav__logo text__color">Justin Cruzana</div>
         <ul className="nav__links">
-          <li className="nav__link">Home</li>
-          <li className="nav__link">Contact</li>
-          <li className="nav__link" onClick={() => handleClick(works)}>
-            Works
-          </li>
+          <li className="nav__link link__hover-effect" onClick={() => handleClick(home)}>Home</li>
+          <li className="nav__link link__hover-effect" onClick={() => handleClick(works)}>Works</li>
+          <li className="nav__link main__link">
+						<a href="mailto:justinandrewcruzana@gmail.com" >Contact</a>	
+					</li>
         </ul>
-        <button className="burger__menu">
+        <button className="burger__menu" onClick={onClickMenu}>
           <FontAwesomeIcon icon="bars" />
         </button>
-        <div className="menu__backdrop"></div>
+				{ load && 
+					<div className="menu__backdrop">
+          <button className="menu__exit" onClick={onClickMenu}>
+            <FontAwesomeIcon icon="times" />
+          </button>
+          <ul className="menu__links">
+            <li className="menu__link" onClick={() => handleClick(home)}>Home</li>
+            <li className="menu__link">
+							<a href="mailto:justinandrewcruzana@gmail.com" >Contact</a>	
+						</li>
+            <li className="menu__link" onClick={() => handleClick(works)}>
+              Works
+            </li>
+          </ul>
+        </div>
+				}
       </div>
     </nav>
   );
