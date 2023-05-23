204c44f891cff573cce724c72539bbccbf6ce05f
Phil <pk.oaferina@gmail.com>
2023-05-23 17:26:45 +0400

    fix link

--- src/components/ui/Project.jsx
+++ src/components/ui/Project.jsx
@@ -3,13 +3,14 @@ function Project({ info }) {
   return (
     <div className="project keen-slider__slide">
       <div className="project__content--wrapper">
-        <h2 className="project__title">TITLE</h2>
-        <p className="project__summary">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus,
-          itaque?
-        </p>
+        <h2 className="project__title">{info.title}</h2>
+        <p className="project__summary">{info.summary}</p>
 				<div className="project__links">
-					<div className="project__link">link</div>
+					<div className="project__link">
+						<a href={info.link} target="_blank" rel="noopener noreferrer">
+							Read Here!
+						</a>
+					</div>
 				</div>
       </div>
 			<figure className="project__img--wrapper ">
